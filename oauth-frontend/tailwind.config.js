9e3d65c5cd9fbbd6be5b416aaca79db5537ef512
rafaapcode <rafaap2003@gmail.com>
2024-10-06 21:30:49 -0300

    feat: setting up the tutorial frontend

---
+++ oauth-frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
